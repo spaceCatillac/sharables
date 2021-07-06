66f27c0f0437cd6962340588d90c6a483c402f2b
spaceCatillac <86897254+spaceCatillac@users.noreply.github.com>
2021-07-05 23:21:55 -0700

    Add files via upload
    Set this entire python script as a hotkey to quickly mute all channels or selected channels.

---
+++ muteChansSimple.py
@@ -0,0 +1,37 @@
+from maya import cmds
+def getSelectedChannels():
+    if not cmds.ls(sl=True):
+        return
+    gChannelBoxName = mel.eval('$temp=$gChannelBoxName')
+    sma = cmds.channelBox(gChannelBoxName, query=True, sma=True)
+    ssa = cmds.channelBox(gChannelBoxName, query=True, ssa=True)
+    sha = cmds.channelBox(gChannelBoxName, query=True, sha=True)
+    channels = list()
+    if sma:
+        channels.extend(sma)
+    if ssa:
+        channels.extend(ssa)
+    if sha:
+        channels.extend(sha)
+    return channels 
+chans = getSelectedChannels()
+
+if not chans:
+    objs = cmds.ls(sl=True)
+    muteState = cmds.mute('{}.tx'.format(objs[0]), q=True)
+    if not muteState:
+        cmds.mute(objs, force=True)
+    else:
+        cmds.mute(objs, disable=True, force=True)
+else:
+    attrsToMute = []
+    objs = cmds.ls(sl=True)
+    for obj in objs:
+        for chan in chans:
+            attr = '{}.{}'.format(obj, chan)
+            attrsToMute.append(attr)
+    muteState = cmds.mute(attrsToMute[0], q=True)
+    if not muteState:
+        cmds.mute(attrsToMute, force=True)
+    else:
+        cmds.mute(attrsToMute, disable=True, force=True)
